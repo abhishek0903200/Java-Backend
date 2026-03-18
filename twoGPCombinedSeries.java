ba41a846810f472ec0e9b23d5ae56b36d7bc74e4
adding files <abhishek.20ec106@jssaten.ac.in>
2026-03-18 10:45:10 +0530

    Initial commit with Java preparation files

---
+++ twoGPCombinedSeries.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class twoGPCombinedSeries {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result;
+
+        if (n % 2 == 0) {
+            int power = (n - 2) / 2;
+            result = (int) Math.pow(3, power);
+        }
+
+        else {
+            int power = (n - 1) / 2;
+            result = (int) Math.pow(2, power);
+        }
+
+        System.out.println(result);
+
+    }
+
+}
